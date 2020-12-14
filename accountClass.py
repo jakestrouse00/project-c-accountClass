1f46673da85aa1dffbc52b99fcfe4ae90b740372
Jake <jakestrouse00@gmail.com>
2020-12-14 11:12:25 -0500

    Create accountClass.py

---
+++ accountClass.py
@@ -0,0 +1,17 @@
+class Account:
+  def __init__(self, first_name: str, last_name: str, country: str, address_1: str, address_2: str, city: str, zipcode: int, state: str, phone: str, card_num: int, card_type: str, card_cvv: int, card_month: int, card_year: int)
+      self.first_name = first_name
+      self.last_name = last_name
+      self.country = country
+      self.address_1 = address_1
+      self.address_2 = address_2
+      self.city = city
+      self.zipcode = zipcode
+      self.state = state
+      self.phone = phone
+      self.card_num = card_num
+      self.card_type = card_type
+      self.card_cvv = card_cvv
+      self.card_month = card_month
+      self.card_year = card_year
+      
